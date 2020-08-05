6be7dd1d73cddc9b80655f53b5f614cc75035e17
sakshi <dhasmanasakshi@gmail.com>
2020-08-05 15:08:47 +0530

    Div module added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ System.out.println("Sum ="+(a+b));
   public void diff(int a,int b){
 System.out.println("Difference ="+(a-b));
 }
+  public void div(int a,int b){
+System.out.println("Quotient ="+(a/b));
+}
 }
