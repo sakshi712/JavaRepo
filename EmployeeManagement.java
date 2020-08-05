70b87874075de045ac0884d5963b2d4967b6a876
sakshi <dhasmanasakshi@gmail.com>
2020-08-05 15:03:59 +0530

    Product function added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ System.out.println("Sum ="+(a+b));
   public void diff(int a,int b){
 System.out.println("Difference ="+(a-b));
 }
+  public void prod(int a,int b){
+System.out.println("Product ="+(a*b));
+}
 }
